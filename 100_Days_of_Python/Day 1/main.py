c7be2c533ce4c99b8c92f39306741e005a17f456
tynaalban <justinaudofia@gmail.com>
2024-05-12 12:43:52 +0100

    Create main.py
    Printing to Console

---
+++ 100_Days_of_Python/Day 1/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 09:59:58 2024
+
+@author: Tyna
+"""
+
+print("Justina")
+print("5th May, 2024")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling 😊")
+print("You can follow my progress at replit.com/@justinaudofia")
