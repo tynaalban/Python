4547d5da5cf2f3e7fa8706f54ae88807f4476e6a
tynaalban <justinaudofia@gmail.com>
2024-05-25 09:09:23 +0100

    Printing

--- 100_Days_of_Python/Day 1/main.py
+++ 100_Days_of_Python/Day 1/main.py
@@ -1,5 +1,5 @@
 print("Justina")
-print("5th May, 2024")
+print("25th May, 2024")
 print("""I am signing up for Replit's 100 days of Python challenge!
 I will make sure to spend some time every day coding along, for a minimum of 60 minutes a day.
 I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
